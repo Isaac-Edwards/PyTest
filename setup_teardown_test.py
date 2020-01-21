1a17ee44c4445be8059ec5fedb9e5907307edf9d
Isaac Edwards <isaac.edwards@displaylink.com>
2020-01-21 11:51:36 +0000

    Setup and teardown class

--- setup_teardown_test.py
+++ setup_teardown_test.py
@@ -4,30 +4,39 @@ def setup_module(module):
 def teardown_module(module):
     print("Teardown Module!")
 
-def setup_function(function):
-    if function == test1:
-        print("\nSetting up test1!")
-    if function == test2:
-        print("\nSetting up test2!")
-    else:
-        print("\nSetting up something else...")
+class TestClass:
+    @classmethod
+    def setup_class(cls):
+        print("Setup TestClass!")
+    
+    @classmethod
+    def teardown_class(cls):
+        print("Teardown TestClass!")
 
-def teardown_function(function):
-    if function == test1:
-        print("\nTearing down test1!")
-    if function == test2:
-        print("\nTearing down test2!")
-    else:
-        print("\nTearing down something else...")
+    def setup_method(self, method):
+        if method == self.test1:
+            print("\nSetting up test1!")
+        if method == self.test2:
+            print("\nSetting up test2!")
+        else:
+            print("\nSetting up something else...")
 
-def test1():
-    print("Executing test1!")
-    assert True
+    def teardown_function(self, method):
+        if method == self.test1:
+            print("\nTearing down test1!")
+        if method == self.test2:
+            print("\nTearing down test2!")
+        else:
+            print("\nTearing down something else...")
 
-def test2():
-    print("Executing test2!")
-    assert True
+    def test1(self):
+        print("Executing test1!")
+        assert True
 
-def test3():
-    print("Executing test3!")
-    assert False
+    def test2(self):
+        print("Executing test2!")
+        assert True
+
+    def test3(self):
+        print("Executing test3!")
+        assert False
