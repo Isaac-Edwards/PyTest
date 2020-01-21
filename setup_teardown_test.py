2c389c5678bea59b5f3315066cd78704eafa0bd8
Isaac Edwards <isaac.edwards@displaylink.com>
2020-01-21 11:46:01 +0000

    setup and teardown examples

---
+++ setup_teardown_test.py
@@ -0,0 +1,27 @@
+def setup_function(function):
+    if function == test1:
+        print("\nSetting up test1!")
+    if function == test2:
+        print("\nSetting up test2!")
+    else:
+        print("\nSetting up something else...")
+
+def teardown_function(function):
+    if function == test1:
+        print("\nTearing down test1!")
+    if function == test2:
+        print("\nTearing down test2!")
+    else:
+        print("\nTearing down something else...")
+
+def test1():
+    print("Executing test1!")
+    assert True
+
+def test2():
+    print("Executing test2!")
+    assert True
+
+def test3():
+    print("Executing test3!")
+    assert False
