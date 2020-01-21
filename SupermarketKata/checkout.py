c9c3a8b7a4cc92f22901e2aa491f7571600ec80f
Isaac Edwards <isaac.edwards@displaylink.com>
2020-01-21 12:55:12 +0000

    Third test case: add item

--- SupermarketKata/checkout.py
+++ SupermarketKata/checkout.py
@@ -1,5 +1,8 @@
 class Checkout:
-    def __init__(self): 
+    def __init__(self):
+        pass
+
+    def add_item(self, name):
         pass
 
     def add_item_price(self, name, price):
--- SupermarketKata/test_checkout.py
+++ SupermarketKata/test_checkout.py
@@ -1,5 +1,13 @@
+import pytest
 from checkout import Checkout
 
-def test_can_add_item_price():
+@pytest.fixture()
+def checkout():
     checkout = Checkout()
+    return checkout
+
+def test_can_add_item_price(checkout):
     checkout.add_item_price("bananas", "0.20")
+
+def test_can_add_item(checkout):
+    checkout.add_item("bananas")
