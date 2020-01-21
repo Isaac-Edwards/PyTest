7ae762a185166890452284e5d94ba2626393114e
Isaac Edwards <isaac.edwards@displaylink.com>
2020-01-21 13:25:00 +0000

    sixth requirement: can add discount

--- SupermarketKata/checkout.py
+++ SupermarketKata/checkout.py
@@ -9,5 +9,8 @@ class Checkout:
     def add_item_price(self, name, price):
         self.prices[name] = price
 
+    def add_discount(self, name, number_of_items, price):
+        pass
+
     def subtotal(self):
         return self.total
--- SupermarketKata/test_checkout.py
+++ SupermarketKata/test_checkout.py
@@ -4,36 +4,36 @@ from checkout import Checkout
 @fixture()
 def checkout():
     checkout = Checkout()
+    checkout.add_item_price("banana", 0.20)
+    checkout.add_item_price("milk", 1)
+    checkout.add_item_price("TV", 999)
     return checkout
 
 def test_can_calculate_current_total(checkout):
-    checkout.add_item_price("bananas", 0.20)
-    checkout.add_item("bananas")
+    checkout.add_item("banana")
     subtotal = checkout.subtotal()
     assert subtotal == approx(0.20)
 
 def test_can_calculate_total_for_multiple_items(checkout):
-    checkout.add_item_price("bananas", 0.20)
-    checkout.add_item_price("milk", 1)
-    checkout.add_item_price("TV", 999)
-
-    checkout.add_item("bananas")
+    checkout.add_item("banana")
     checkout.add_item("milk")
     checkout.add_item("TV")
-    
+
     subtotal = checkout.subtotal()
     assert subtotal == approx(1000.20)
 
 def test_can_add_price_but_not_item_and_total_stays_zero(checkout):
-    checkout.add_item_price("bananas", 0.20)
+    checkout.add_item_price("banana", 0.20)
     subtotal = checkout.subtotal()
     assert subtotal == 0
 
 def test_when_same_item_added_multiple_times_subtotal_is_correct(checkout):
-    checkout.add_item_price("banana", 0.20)
     checkout.add_item("banana")
     checkout.add_item("banana")
     checkout.add_item("banana")
     checkout.add_item("banana")
     subtotal = checkout.subtotal()
     assert subtotal == approx(0.80)
+
+def test_can_add_discount_rules(checkout):
+    checkout.add_discount("banana", 3, 2)
